5a76d21380771bfcaf189ffd1e12bec16fefdcab
Montbra <montbra@gmail.com>
2018-11-01 21:59:14 -0300

    Add some logic

--- Tutorial.py
+++ Tutorial.py
@@ -5,11 +5,16 @@ game = [[0,0,0],
 
 #printing a column index, printing a row index, now #we have a coordinate to make a move, like c2
 
-def game_board():
-    print('   a  b  c')
+def game_board(player=0, row=1, column=1, just_display=False):
+    print('   1  2  3')
+    if not just_display:
+        game[row][column] = player
     for count, row in enumerate(game, 1):
         print(count, row)
 
-game[0][1] = 1
+game_board(just_display=True)
+game_board(player=1, row=2, column=1)
 
-game_board()
+# game[0][1] = 1
+
+# game_board()
